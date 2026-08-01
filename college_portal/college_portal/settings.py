d566f82adde67a4e877c970fec414cb32f93e759
Vaibhav Yadav <vaibhavyadav6966@gmail.com>
2026-08-01 10:00:18 +0530

    Prepare project for Render deployment

--- college_portal/college_portal/settings.py
+++ college_portal/college_portal/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-q(0ejs*t5-+$*6tv#tw8-xym*h4rko#a*ne=sa7dv5-#uxxwez
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -186,3 +187,7 @@ JAZZMIN_UI_TWEAKS = {
         "danger": "btn-danger",
     },
 }
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
